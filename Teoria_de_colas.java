d2f9e7693068caf38b7c4b7dca11a06e2520559b
Sadai Lazca <zuribonilla234@gmail.com>
2025-12-04 07:51:29 +0000

    se agrego la clase y el menu

--- Teoria_de_colas.java
+++ Teoria_de_colas.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Teoria_de_colas{
     public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        byte opcion=0;
+        do{
+            System.out.println("--Teoria de colas--");
+            System.out.println("1. Modelo m/m/1");
+            System.out.println("2. Modelo m/m/1/k");
+            System.out.println("3. Modelo m/m/c");
+            System.out.println("0. salir del programa");
+            opcion=entrada.nextByte();
+            
+
+        }while (opcion !=0);
         
     }
 }
